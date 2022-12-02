4528c0352cb3fe89ba2012196bdea8860956b1c8
DiegoM <medeldiego47@yahoo.com>
2022-12-01 22:55:30 -0600

    created user controller functions

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const { Thought, User } = require("../models");
+
+const userControllers= {
+
+getAllUsers(req,res){
+    User.find().select('__v').then(Users=> res.json(Users)).catch(err=>{console.log(err); res.sendStatus(400)})},
+
+getUserById(req,res){
+    User.findOne({_id: req.params.id}).then(userData=> !userData ?res.status(404).json({message: "no user matches this ID "}): res.json(userData))},
+
+createNewUser(req,res){
+    User.create(req.body).then(userData=> res.json(userData)).catch(err => res.status(500).json(err))},
+
+updateUser(req,res){User.findOneAndUpdate({_id:req.params.id},
+    {$set:req.body},
+    {runValidators:true,new:true})
+    .then(userData=> !userData? res.status(404).json({message: "No user found with this id"}):res.json(userData))
+},
+
+deleteUser(req,res){User.findOneAndDelete({_id:req.params.id}).then(userData => 
+    !userData? res.status(404).json({message:"no matching user"}): res.json({message:'User has been deleted'}))},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
